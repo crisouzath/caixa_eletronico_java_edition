b9ea1a666f03b9608d84edb458a43a2b930b51e1
Cristhian de Souza <111239606+crisouzath@users.noreply.github.com>
2022-12-03 23:10:39 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,141 +2,33 @@ import java.util.Scanner;
 
 class Main {
   static double saldo = 100;
-  static String nome;
+  static Scanner scanner = new Scanner(System.in);
   static int senha = 3589;
-
-  public static void ver_saldo() {
-    verificar_senha();
-    System.out.println("Seu saldo atual é: " + saldo);
-    main(null);
-  }
+  static String usuario;
 
   public static void login() {
-    System.out.println("Informe seu nome:");
-    Scanner resposta = new Scanner(System.in);
-    String usuario = resposta.nextLine();
-    nome = usuario;
-    System.out.println("Olá, " + nome + " é um prazer ter você aqui.");
-    resposta.close();
+    System.out.println("Nome:");
+    usuario = scanner.nextLine();
   }
 
   public static void verificar_senha() {
-    System.out.println("Informe a senha:");
-    Scanner resposta = new Scanner(System.in);
-    int senha_digitada = resposta.nextInt();
-    if (senha_digitada == senha) {
-      System.out.println("Senha correta");
-    } else {
-      main(null);
-    }
-    resposta.close();
-  }
-
-  public static void fazer_deposito() {
-    verificar_senha();
-    System.out.println("Qual o valor a ser depositado?");
-    Scanner valor = new Scanner(System.in);
-    Double deposito = valor.nextDouble();
-
-    boolean checaNumero = deposito.isNaN(); 
-    if (checaNumero || deposito <= 0) {
-      System.out.println("Por favor, informe um número válido:");
-      fazer_deposito();
-    } else {
-      saldo += deposito;
-      ver_saldo();
-    }
-
-    valor.close();
-  }
-
-  public static void fazer_saque() {
-    verificar_senha();
-    System.out.println("Qual o valor para saque?");
-    Scanner valor = new Scanner(System.in);
-    Double saque = valor.nextDouble();
-
-    boolean checaNumero = saque.isNaN();
-
-    if (checaNumero || saque < 0) {
-      System.out.println("Por favor, informe um número válido:");
-      fazer_saque();
-    } else if (saque > saldo) {
-      System.out.println("Operação não autorizada");
-      fazer_saque();
-    } else {
-      saldo -= saque;
-      ver_saldo();
-
-      valor.close();
+    System.out.println("Senha:");
+    int checaSenha = scanner.nextInt();
+    while (checaSenha != senha) {
+      System.out.println("Senha incorreta");
+      checaSenha = scanner.nextInt();
     }
   }
 
-  public static void ver_extrato() {
-    verificar_senha();
-    System.out.println("Saque de R$300,00. Depósito de R$4000");
-    main(null);
-  }
+  public static void opcoes() {
+    System.out.println(
+        "Selecione uma opção:\n 1.) Saldo;\n 2.) Extrato;\n 3.) Saque;\n 4.) Depósito;\n 5.) Transferência;\n 6.) Sair. ");
 
-  public static void transferencia() {
-    verificar_senha();
-    System.out.println("Qual o valor para transferencia?");
-    Scanner valor = new Scanner(System.in);
-    Double transferencia_valor = valor.nextDouble();
+    int opcao = scanner.nextInt();
 
-     System.out.println("Para qual conta tranferir?");
-     Scanner conta = new Scanner(System.in);
-     Double numero_conta = conta.nextDouble();
-
-    boolean checaNumero = transferencia_valor.isNaN();
-    boolean checarConta= numero_conta.isNaN();
-
-    if (checaNumero || checarConta || transferencia_valor <= 0 || transferencia_valor > saldo) {
-      System.out.println("Por favor, informe um número válido:");
-      transferencia();
-    } else {
-      saldo -= transferencia_valor;
-      ver_saldo();
-
-      valor.close();
-    }
-  }
-
-  public static void erro() {
-    System.out.println("Por favor, informe um número entre 1 e 6");
-    main(null);
-  }
-
-  public static void sair() {
-    System.out.println("Você deseja sair? S/N");
-
-    Scanner sair = new Scanner(System.in);
-    String escolha = sair.nextLine();
-
-    if (escolha.equals("S")) {
-      System.out.println(nome + ", foi um prazer ter você por aqui.");
-      System.exit(0);
-    } else if (escolha.equals("N")) {
-      main(null);
-    } else {
-      System.out.println("Desculpe, mas não compreendi.");
-      sair();
-    }
-
-    sair.close();
-
-  }
-
-  public static void main(String[] args) {
-    login();
-
-    System.out.println("Selecione uma opção 1.) Saldo 2.) Depósito 3.) Saque 4.) Extrato 5. ) Transferência 6. ) Sair");
-
-    Scanner in = new Scanner(System.in);
-    int escolha = in.nextInt();
-
-     switch(escolha){
+    switch (opcao) {
       case 1:
+        verificar_senha();
         ver_saldo();
         break;
       case 2:
@@ -146,19 +38,113 @@ class Main {
         fazer_saque();
         break;
       case 4:
-         fazer_deposito();
+        fazer_deposito();
         break;
       case 5:
-         transferencia();
-         break;
+        fazer_transferencia();
+        break;
       case 6:
         sair();
-        break;
       default:
         erro();
+
+    }
+  }
+
+  public static void ver_saldo() {
+
+    System.out.println("Seu saldo atual é: " + saldo);
+
+    opcoes();
+  }
+
+  private static void ver_extrato() {
+    verificar_senha();
+    System.out.println("Seu extrato:\n Tranferência: -100\n Pix para Vladimilson: -150\n Pix recebido: +100");
+    opcoes();
+  }
+
+  public static void fazer_saque() {
+    verificar_senha();
+    System.out.println("Qual o valor para saque?");
+    Double saque = scanner.nextDouble();
+
+    boolean checaNumero = saque.isNaN();
+
+    if (checaNumero || saque < 0 || saldo < saque) {
+      System.out.println("Operação não realizada.");
+      fazer_saque();
+    } else {
+      saldo -= saque;
+      ver_saldo();
+    }
+  }
+
+  public static void fazer_deposito() {
+    verificar_senha();
+
+    System.out.println("Qual o valor a ser depositado?");
+    Double deposito = scanner.nextDouble();
+
+    boolean checaNumero = deposito.isNaN();
+    if (checaNumero || deposito <= 0) {
+      System.out.println("Operação não realizada.");
+      fazer_deposito();
+    } else {
+      saldo += deposito;
+      ver_saldo();
+    }
+  }
+
+  private static void fazer_transferencia() {
+    verificar_senha();
+
+    System.out.println("Informe a conta de destino");
+    Double conta = scanner.nextDouble();
+    boolean checaConta = conta.isNaN();
+    if (checaConta) {
+      System.out.println("Operação não realizada.");
+      fazer_transferencia();
     }
 
-    in.close();
+    System.out.println("Qual o valor da transferência?");
+    Double saque = scanner.nextDouble();
+    boolean checaNumero = saque.isNaN();
+
+    if (checaNumero || saque < 0 || saldo < saque) {
+      System.out.println("Operação não realizada");
+      fazer_transferencia();
+    } else {
+      saldo -= saque;
+      ver_saldo();
+    }
+  }
+
+  public static void erro() {
+    System.out.println("Por favor, informe um número entre 1 e 6");
+    opcoes();
+  }
+
+  public static void sair() {
+    System.out.println("Você deseja sair? S/N");
+    String escolha = scanner.nextLine();
+
+    if (escolha.equals("S")) {
+      System.out.println(usuario + ", foi um prazer ter você por aqui!");
+      System.exit(0);
+    } else if (escolha.equals("N")) {
+      opcoes();
+    } else {
+      System.out.println("Desculpe, mas não compreendi.");
+      sair();
+    }
+
+  }
+
+  public static void main(String[] args) {
+    login();
+    System.out.println("Olá " + usuario + " é um prazer ter você por aqui!");
+    opcoes();
   }
 
 }
